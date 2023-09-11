6abf11423053fb178b38ba25f9915fb2847d3c31
Mario-Marchio <mariomarchio1610@gmail.com>
2023-09-12 01:12:15 +0200

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = ['title', 'image', 'content'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
+    public function tecnologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
+}
